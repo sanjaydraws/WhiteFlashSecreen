28a03d35ecb155e574ceaab39e823f1409cbd65d
sanjaydraws <sprajapat8331@gmail.com>
2022-07-04 07:32:47 +0530

    dark and light image using color filter

--- app/src/main/java/com/example/whitescreen/MainActivity.kt
+++ app/src/main/java/com/example/whitescreen/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.whitescreen
 
 import android.content.ContentResolver
 import android.content.Intent
+import android.graphics.LightingColorFilter
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.WindowManager
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.whitescreen.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
@@ -17,14 +19,10 @@ import kotlin.system.exitProcess
 class MainActivity : AppCompatActivity() {
     var binding:ActivityMainBinding? = null
     val TAG = "MainActivity"
-    //Variable to store brightness value
-    private var brightness = 0
-
-    //Content resolver used as a handle to the system's settings
-    private val cResolver: ContentResolver? = null
-
-    //Window object, that will store a reference to the current window
-//    private var window: Window? = null
+    var multipler_bulb = 0
+    var addition_bulb = 0
+    var multipler_bb = 0
+    var addition_bb = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
@@ -66,24 +64,41 @@ class MainActivity : AppCompatActivity() {
 
 //        binding?.seekBar?.progress = brightness
 
+        setInitialProgress()
+        binding?.seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                multipler_bb = spread(progress)
+//                setLighting()
+                settingLightTuned(progress)
 
-
-//        binding?.seekBar?.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-//            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-//
-//                Settings.System.putInt(
-//                    applicationContext.contentResolver,
-//                    Settings.System.SCREEN_BRIGHTNESS, brightness.toInt()
-//                )
-//            }
-//
-//            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-//            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-//        })
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+    }
+
+    private fun spread(progress: Int) = (progress * 0x10000 + progress * 0x100 + progress * 0x1)
+    private fun setLighting() {
+        binding?.imageBackground?.colorFilter = LightingColorFilter(multipler_bb, addition_bb)
+//        binding?.imageBackground?.colorFilter = LightingColorFilter(multipler_bulb, addition_bulb)
+    }
+    private fun settingLightTuned(progress: Int) {
+        val bulbMul = spread((progress / 255f * 255f + 0).toInt())
+        val bulbAdd = spread((progress / 255f * 200f + 0).toInt())
+        val bbMul = spread((progress / 255f * 105f + 150).toInt())
+        val bbAdd = spread((progress / 255f * 50f + 0).toInt())
+
+        binding?.imageBackground?.colorFilter = LightingColorFilter(bbMul, bbAdd)
+//        image_bulb.colorFilter = LightingColorFilter(bulbMul, bulbAdd)
+    }
+
+    private fun setInitialProgress(){
+        settingLightTuned(255)
+        binding?.seekBar?.progress = 255
     }
 }
